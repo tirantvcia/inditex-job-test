6adb89e2214aa2a62992d500dffc0aef86ec763c
tirantvcia <tirantvcia@gmail.com>
2023-06-03 23:44:08 +0200

    green, petición a las 21:00 del día 16 del producto 35455 para la brand 1

--- src/test/java/com/inditex/test/springboot/app/SpringBootJpaTest.java
+++ src/test/java/com/inditex/test/springboot/app/SpringBootJpaTest.java
@@ -10,18 +10,12 @@ import java.time.LocalTime;
 import java.time.ZoneId;
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.domain.Sort.Order;
 
-import com.inditex.test.springboot.app.data.ProductRate;
 import com.inditex.test.springboot.app.data.RateSelection;
 import com.inditex.test.springboot.app.models.Price;
 import com.inditex.test.springboot.app.repositories.PricesRepository;
@@ -134,6 +128,25 @@ public class SpringBootJpaTest {
 		assertEquals(1, price.getBrand().getId());
 		assertEquals(30.50, price.getPrice());
 	}
+	
+	@Test
+	@DisplayName("petición a las 21:00 del día 16 del producto 35455 para la brand 1")
+	void checkProductRatesForDate16at21Oclock() {
+		
+		
+		RateSelection entry = new RateSelection(LocalDate.of(2020, 6, 16),
+					LocalTime.of(21,00), 35455L, 1L);
+		
+		List<Price> findFirstPriceRateBySelectionEntry = 
+				pricesRepository.findPriceRatesBySelectionEntry(entry.getDate(), entry.getProduct(), entry.getBrand());
+		
+		Price price =  findFirstPriceRateBySelectionEntry.stream().findFirst().orElse(new Price());	
+		
+		assertEquals(35455L, price.getProductId());
+		assertEquals(4, price.getId());
+		assertEquals(1, price.getBrand().getId());
+		assertEquals(38.95, price.getPrice());
+	}	
 	private Date convertToDate(LocalDateTime dateToConvert) {
 	    return java.util.Date
 	      .from(dateToConvert.atZone(ZoneId.systemDefault())
