a1c55eeee3a7c63b919d450bb5b3cf28bafe0f40
tirantvcia <tirantvcia@gmail.com>
2023-06-04 23:55:02 +0200

    refactor, remove unused imports

--- src/main/java/com/inditex/test/springboot/app/controllers/PriceListController.java
+++ src/main/java/com/inditex/test/springboot/app/controllers/PriceListController.java
@@ -1,9 +1,5 @@
 package com.inditex.test.springboot.app.controllers;
 
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.Date;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,8 +10,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.inditex.test.springboot.app.data.ProductRate;
-import com.inditex.test.springboot.app.models.Brand;
-import com.inditex.test.springboot.app.models.Price;
 import com.inditex.test.springboot.app.services.Facade;
 
 @RestController
