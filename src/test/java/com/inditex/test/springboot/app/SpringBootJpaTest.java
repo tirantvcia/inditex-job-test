b532b97c04a414fb0b97c3b6b1711b5594e05d9c
José Pascual Gimeno Marí <tirantvcia@gmail.com>
2023-06-05 23:29:45 +0200

    refactor, implements assertions with TestData constants

--- src/test/java/com/inditex/test/springboot/app/SpringBootJpaTest.java
+++ src/test/java/com/inditex/test/springboot/app/SpringBootJpaTest.java
@@ -3,6 +3,7 @@ package com.inditex.test.springboot.app;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -20,6 +21,9 @@ import com.inditex.test.springboot.app.repositories.PricesRepository;
 
 @DataJpaTest
 public class SpringBootJpaTest {
+	private static final long BRAND_ID_1 = 1L;
+	private static final long PRODUCT_ID_35455 = 35455L;
+	
 	@Autowired
 	PricesRepository pricesRepository;
 
@@ -27,8 +31,9 @@ public class SpringBootJpaTest {
 	@DisplayName("Return empty rate when priceList is not selected")
 	void emptyProductRateForNoPriceSelection() {
 
-		RateSelection entry = RateSelection.create("14-03-2022", "10:00", 34455L, 1L);
-
+		String date = "14-03-2022";
+		String time = "10:00";
+		RateSelection entry = RateSelection.create(date, time, PRODUCT_ID_35455, BRAND_ID_1);
 		List<Price> findFirstPriceRateBySelectionEntry = pricesRepository
 				.findPricesBySelectionOrderedByPrioriry(entry.getDate(), entry.getProduct(), entry.getBrand());
 
@@ -49,91 +54,103 @@ public class SpringBootJpaTest {
 	@DisplayName("petición a las 10:00 del día 14 del producto 35455   para la brand 1")
 	void checkProductRatesForDate14at10Oclock() {
 
-		RateSelection entry = RateSelection.create("14-06-2020", "10:00", 35455L, 1L);
+		String date = "14-06-2020";
+		String time = "10:00";
+		RateSelection entry = RateSelection.create(date, time, PRODUCT_ID_35455, BRAND_ID_1);
 
 		List<Price> findFirstPriceRateBySelectionEntry = pricesRepository
 				.findPricesBySelectionOrderedByPrioriry(entry.getDate(), entry.getProduct(), entry.getBrand());
 
+		assertTrue(findFirstPriceRateBySelectionEntry.size() > 0);
 		Price price = findFirstPriceRateBySelectionEntry.stream().findFirst().orElse(new Price());
-
-		assertEquals(35455L, price.getProductId());
-		assertEquals(1, price.getId());
-		assertEquals(1, price.getBrand().getId());
-		assertEquals(convertToDate(LocalDateTime.of(2020, 6, 14, 0, 0, 0)), price.getStartDate());
-		assertEquals(convertToDate(LocalDateTime.of(2020, 12, 31, 23, 59, 59)), price.getEndDate());
-		assertEquals(35.50, price.getPrice());
+		assertEquals(TestData.PRICE_LIST_1.getProductId(), price.getProductId());
+		assertEquals(TestData.PRICE_LIST_1.getId(), price.getId());
+		assertEquals(TestData.PRICE_LIST_1.getBrand().getId(), price.getBrand().getId());
+		assertTrue(TestData.PRICE_LIST_1.getStartDate().equals(price.getStartDate()));
+		assertTrue(TestData.PRICE_LIST_1.getEndDate().equals(price.getEndDate()));
+		assertEquals(TestData.PRICE_LIST_1.getPrice(), price.getPrice());
 	}
 
 	@Test
 	@DisplayName("petición a las 16:00 del día 14 del producto 35455 para la brand 1")
 	void checkProductRatesForDate14at16Oclock() {
 
-		RateSelection entry = RateSelection.create("14-06-2020", "16:00", 35455L, 1L);
+		String date = "14-06-2020";
+		String time = "16:00";
+		RateSelection entry = RateSelection.create(date, time, PRODUCT_ID_35455, BRAND_ID_1);
 
 		List<Price> findFirstPriceRateBySelectionEntry = pricesRepository
 				.findPricesBySelectionOrderedByPrioriry(entry.getDate(), entry.getProduct(), entry.getBrand());
+		
 
+		assertTrue(findFirstPriceRateBySelectionEntry.size() > 0);
 		Price price = findFirstPriceRateBySelectionEntry.stream().findFirst().orElse(new Price());
-
-		assertEquals(35455L, price.getProductId());
-		assertEquals(2, price.getId());
-		assertEquals(1, price.getBrand().getId());
-		assertEquals(25.45, price.getPrice());
+		assertEquals(TestData.PRICE_LIST_2.getProductId(), price.getProductId());
+		assertEquals(TestData.PRICE_LIST_2.getId(), price.getId());
+		assertEquals(TestData.PRICE_LIST_2.getBrand().getId(), price.getBrand().getId());
+		assertTrue(TestData.PRICE_LIST_2.getStartDate().equals(price.getStartDate()));
+		assertTrue(TestData.PRICE_LIST_2.getEndDate().equals(price.getEndDate()));
+		assertEquals(TestData.PRICE_LIST_2.getPrice(), price.getPrice());
 	}
 
 	@Test
 	@DisplayName("petición a las 21:00 del día 14 del producto 35455 para la brand 1")
 	void checkProductRatesForDate14at21Oclock() {
 
-		RateSelection entry = RateSelection.create("14-06-2020", "21:00", 35455L, 1L);
+		String date = "14-06-2020";
+		String time = "21:00";
+		RateSelection entry = RateSelection.create(date, time, PRODUCT_ID_35455, BRAND_ID_1);
 
 		List<Price> findFirstPriceRateBySelectionEntry = pricesRepository
 				.findPricesBySelectionOrderedByPrioriry(entry.getDate(), entry.getProduct(), entry.getBrand());
-
+		assertTrue(findFirstPriceRateBySelectionEntry.size() > 0);
 		Price price = findFirstPriceRateBySelectionEntry.stream().findFirst().orElse(new Price());
-
-		assertEquals(35455L, price.getProductId());
-		assertEquals(1, price.getId());
-		assertEquals(1, price.getBrand().getId());
-		assertEquals(35.50, price.getPrice());
+		assertEquals(TestData.PRICE_LIST_1.getProductId(), price.getProductId());
+		assertEquals(TestData.PRICE_LIST_1.getId(), price.getId());
+		assertEquals(TestData.PRICE_LIST_1.getBrand().getId(), price.getBrand().getId());
+		assertTrue(TestData.PRICE_LIST_1.getStartDate().equals(price.getStartDate()));
+		assertTrue(TestData.PRICE_LIST_1.getEndDate().equals(price.getEndDate()));
+		assertEquals(TestData.PRICE_LIST_1.getPrice(), price.getPrice());
 	}
 
 	@Test
 	@DisplayName("petición a las 10:00 del día 15 del producto 35455 para la brand 1")
 	void checkProductRatesForDate15at10Oclock() {
 
-		RateSelection entry = RateSelection.create("15-06-2020", "10:00", 35455L, 1L);
+		String date = "15-06-2020";
+		String time = "10:00";
+		RateSelection entry = RateSelection.create(date, time, PRODUCT_ID_35455, BRAND_ID_1);
 
 		List<Price> findFirstPriceRateBySelectionEntry = pricesRepository
 				.findPricesBySelectionOrderedByPrioriry(entry.getDate(), entry.getProduct(), entry.getBrand());
-
+		assertTrue(findFirstPriceRateBySelectionEntry.size() > 0);
 		Price price = findFirstPriceRateBySelectionEntry.stream().findFirst().orElse(new Price());
-
-		assertEquals(35455L, price.getProductId());
-		assertEquals(3, price.getId());
-		assertEquals(1, price.getBrand().getId());
-		assertEquals(30.50, price.getPrice());
+		assertEquals(TestData.PRICE_LIST_3.getProductId(), price.getProductId());
+		assertEquals(TestData.PRICE_LIST_3.getId(), price.getId());
+		assertEquals(TestData.PRICE_LIST_3.getBrand().getId(), price.getBrand().getId());
+		assertTrue(TestData.PRICE_LIST_3.getStartDate().equals(price.getStartDate()));
+		assertTrue(TestData.PRICE_LIST_3.getEndDate().equals(price.getEndDate()));
+		assertEquals(TestData.PRICE_LIST_3.getPrice(), price.getPrice());
 	}
 
 	@Test
 	@DisplayName("petición a las 21:00 del día 16 del producto 35455 para la brand 1")
 	void checkProductRatesForDate16at21Oclock() {
 
-		RateSelection entry = RateSelection.create("16-06-2020", "21:00", 35455L, 1L);
+		String date = "16-06-2020";
+		String time = "21:00";
+		RateSelection entry = RateSelection.create(date, time, PRODUCT_ID_35455, BRAND_ID_1);
 
 		List<Price> findFirstPriceRateBySelectionEntry = pricesRepository
 				.findPricesBySelectionOrderedByPrioriry(entry.getDate(), entry.getProduct(), entry.getBrand());
-
+		assertTrue(findFirstPriceRateBySelectionEntry.size() > 0);
 		Price price = findFirstPriceRateBySelectionEntry.stream().findFirst().orElse(new Price());
-
-		assertEquals(35455L, price.getProductId());
-		assertEquals(4, price.getId());
-		assertEquals(1, price.getBrand().getId());
-		assertEquals(38.95, price.getPrice());
-	}
-
-	private Date convertToDate(LocalDateTime dateToConvert) {
-		return java.util.Date.from(dateToConvert.atZone(ZoneId.systemDefault()).toInstant());
+		assertEquals(TestData.PRICE_LIST_4.getProductId(), price.getProductId());
+		assertEquals(TestData.PRICE_LIST_4.getId(), price.getId());
+		assertEquals(TestData.PRICE_LIST_4.getBrand().getId(), price.getBrand().getId());
+		assertTrue(TestData.PRICE_LIST_4.getStartDate().equals(price.getStartDate()));
+		assertTrue(TestData.PRICE_LIST_4.getEndDate().equals(price.getEndDate()));
+		assertEquals(TestData.PRICE_LIST_4.getPrice(), price.getPrice());
 	}
 
 }
