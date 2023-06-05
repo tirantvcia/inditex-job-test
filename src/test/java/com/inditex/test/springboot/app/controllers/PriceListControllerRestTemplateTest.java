94f905ee9a771c60463bb5a51b17e978b6005ba0
José Pascual Gimeno Marí <tirantvcia@gmail.com>
2023-06-05 22:49:06 +0200

    refactor some methods in test

--- src/test/java/com/inditex/test/springboot/app/controllers/PriceListControllerRestTemplateTest.java
+++ src/test/java/com/inditex/test/springboot/app/controllers/PriceListControllerRestTemplateTest.java
@@ -23,73 +23,45 @@ import com.inditex.test.springboot.app.data.ProductRate;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class PriceListControllerRestTemplateTest {
+	private static final String GET_MOST_PRIORITY_PRICE_BY_SELECTION = "/test/prices/getMostPriorityPriceBySelection";
+
 	@Autowired
 	private TestRestTemplate client;
-
-
 	@LocalServerPort
 	private String puerto;
 
 
 	@Test
-	@DisplayName("petición a las 10:00 del día 14 del producto 35455   para la brand 1")
+	@DisplayName("petición a las 10:00 del día 14 del producto 35455 para la brand 1")
 	void getMostPriorityPriceBySelection() throws Exception {
-
-		String url = getUri("/test/prices/getMostPriorityPriceBySelection");
 
 		String date = "14-06-2020";
 		String time = "10:00";
 		Long productId = 35455L;
 		Long brandId = 1L;
-		UriComponentsBuilder builder = initComponentBuilder(url, date, time, productId, brandId);
-
-		HttpEntity requestEntity = getHeaders();
-
-		ResponseEntity<ProductRate> response = involeUriExchange(builder, requestEntity);
-
-		ProductRate productRate = response.getBody();
-		assertNotNull(productRate);
-
-		assertNotNull(response.getBody());
-
+		ResponseEntity<ProductRate> response = invokeUriExchange(date, time, productId, brandId);
 		assertEquals(HttpStatus.OK, response.getStatusCode());
 		assertEquals(MediaType.APPLICATION_JSON, response.getHeaders().getContentType());
-
+		ProductRate productRate = response.getBody();
 		assertNotNull(productRate);
-		
-
 		assertEquals(TestData.PRODUCT_RATE_1.getPrice(), productRate.getPrice());
 		assertEquals(TestData.PRODUCT_RATE_1.getPriceList(), productRate.getPriceList());
 		assertEquals(TestData.PRODUCT_RATE_1.getProductId(), productRate.getProductId());
-
 	}
 
 	@Test
 	@DisplayName("petición a las 16:00 del día 14 del producto 35455 para la brand 1")
 	void checkProductRatesForDate14at16Oclock() {
 
-		String url = getUri("/test/prices/getMostPriorityPriceBySelection");
-
 		String date = "14-06-2020";
 		String time = "16:00";
 		Long productId = 35455L;
 		Long brandId = 1L;
-		UriComponentsBuilder builder = initComponentBuilder(url, date, time, productId, brandId);
-
-		HttpEntity requestEntity = getHeaders();
-
-		ResponseEntity<ProductRate> response = involeUriExchange(builder, requestEntity);
-
-		ProductRate productRate = response.getBody();
-		assertNotNull(productRate);
-		assertNotNull(response.getBody());
-
+		ResponseEntity<ProductRate> response = invokeUriExchange(date, time, productId, brandId);
 		assertEquals(HttpStatus.OK, response.getStatusCode());
 		assertEquals(MediaType.APPLICATION_JSON, response.getHeaders().getContentType());
-
+		ProductRate productRate = response.getBody();
 		assertNotNull(productRate);
-		
-
 		assertEquals(TestData.PRODUCT_RATE_2.getPrice(), productRate.getPrice());
 		assertEquals(TestData.PRODUCT_RATE_2.getPriceList(), productRate.getPriceList());
 		assertEquals(TestData.PRODUCT_RATE_2.getProductId(), productRate.getProductId());
@@ -98,57 +70,40 @@ public class PriceListControllerRestTemplateTest {
 	@Test
 	@DisplayName("petición a las 21:00 del día 14 del producto 35455 para la brand 1")
 	void checkProductRatesForDate14at21Oclock() {
-		String url = getUri("/test/prices/getMostPriorityPriceBySelection");
 
 		String date = "14-06-2020";
 		String time = "21:00";
 		Long productId = 35455L;
 		Long brandId = 1L;
-		UriComponentsBuilder builder = initComponentBuilder(url, date, time, productId, brandId);
-
-		HttpEntity requestEntity = getHeaders();
-
-		ResponseEntity<ProductRate> response = involeUriExchange(builder, requestEntity);
-
-		ProductRate productRate = response.getBody();
-		assertNotNull(productRate);
-
-		assertNotNull(response.getBody());
-
+		ResponseEntity<ProductRate> response =  invokeUriExchange(date, time, productId, brandId);
 		assertEquals(HttpStatus.OK, response.getStatusCode());
 		assertEquals(MediaType.APPLICATION_JSON, response.getHeaders().getContentType());
-
+		ProductRate productRate = response.getBody();
 		assertNotNull(productRate);
-		
-
 		assertEquals(TestData.PRODUCT_RATE_1.getPrice(), productRate.getPrice());
 		assertEquals(TestData.PRODUCT_RATE_1.getPriceList(), productRate.getPriceList());
 		assertEquals(TestData.PRODUCT_RATE_1.getProductId(), productRate.getProductId());
-		
 	}
 
+	private ResponseEntity<ProductRate> invokeUriExchange(String date, String time, Long productId, Long brandId) {
+		String url = getUri(GET_MOST_PRIORITY_PRICE_BY_SELECTION);
+		UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url).queryParam("date", date)
+				.queryParam("time", time).queryParam("productId", productId).queryParam("brandId", brandId);
+		HttpEntity requestEntity = getHeaders();
+		return client.exchange(builder.build().encode().toUri(), HttpMethod.GET,
+				requestEntity, ProductRate.class);
+	}
+	
 	private String getUri(String uri) {
 		return "http://localhost:" + puerto + uri;
 	}
 
-	private ResponseEntity<ProductRate> involeUriExchange(UriComponentsBuilder builder, HttpEntity requestEntity) {
-		ResponseEntity<ProductRate> response = client.exchange(builder.build().encode().toUri(), HttpMethod.GET,
-				requestEntity, ProductRate.class);
-		return response;
-	}
 
 	private HttpEntity getHeaders() {
 		HttpHeaders headers = new HttpHeaders();
 		headers.set("accept", "application/json");
 		HttpEntity requestEntity = new HttpEntity<>(headers);
 		return requestEntity;
-	}
-
-	private UriComponentsBuilder initComponentBuilder(String url, String date, String time, Long productId,
-			Long brandId) {
-		UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url).queryParam("date", date)
-				.queryParam("time", time).queryParam("productId", productId).queryParam("brandId", brandId);
-		return builder;
 	}
 
 }
