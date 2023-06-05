796692c4f9d06c295f78c4a278e47853b35f9d71
José Pascual Gimeno Marí <tirantvcia@gmail.com>
2023-06-05 14:46:10 +0200

    green, FacadeTest with Mockito

--- src/main/java/com/inditex/test/springboot/app/services/Facade.java
+++ src/main/java/com/inditex/test/springboot/app/services/Facade.java
@@ -1,44 +1,9 @@
 package com.inditex.test.springboot.app.services;
 
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.Date;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import com.inditex.test.springboot.app.data.ProductRate;
-import com.inditex.test.springboot.app.data.RateSelection;
-import com.inditex.test.springboot.app.models.Brand;
-import com.inditex.test.springboot.app.models.Price;
 
-@Component
-public class Facade {
+public interface Facade {
 
-	private ZaraProductRateService service;
+	ProductRate findMostPriorityPriceBySelection(String date, String time, Long productId, Long brandId);
 
-	public Facade() {
-	}
-
-	@Autowired
-	public Facade(ZaraProductRateService service) {
-		this.service = service;
-	}
-
-	public ProductRate findMostPriorityPriceBySelection(String date, String time, Long productId, Long brandId) {
-
-
-		RateSelection entry = RateSelection.create(date, time, productId, brandId);
-		Brand Brand_ZARA = new Brand(1L, "ZARA");
-		Price PRICE_LIST_1 = new Price(1L, Brand_ZARA, convertToDate(LocalDateTime.of(2020, 6, 14, 0, 0, 0)),
-				convertToDate(LocalDateTime.of(2020, 12, 31, 23, 59, 59)), 35455L, 0, 35.5, "EUR");
-		ProductRate PRODUCT_RATE_1 = ProductRate.create(PRICE_LIST_1);
-		return PRODUCT_RATE_1;
-
-	}
-
-
-	private Date convertToDate(LocalDateTime dateToConvert) {
-		return java.util.Date.from(dateToConvert.atZone(ZoneId.systemDefault()).toInstant());
-	}
 }
--- src/test/java/com/inditex/test/springboot/app/services/FacadeTest.java
+++ src/test/java/com/inditex/test/springboot/app/services/FacadeTest.java
@@ -2,26 +2,28 @@ package com.inditex.test.springboot.app.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.inditex.test.springboot.app.TestData;
 import com.inditex.test.springboot.app.data.ProductRate;
 import com.inditex.test.springboot.app.data.RateSelection;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class FacadeTest {
 
-	@MockBean
+	@Mock
 	ZaraProductRateService service;
 
-	@Autowired
-	Facade facade;
+	@InjectMocks
+	FacadeImpl facade;
 
 	@Test
 	@DisplayName("petición a las 16:00 del día 14 del producto 35455 para la brand 1")
@@ -32,9 +34,8 @@ class FacadeTest {
 		Long productId = 35455L;
 		Long brandId = 1L;
 		
-		RateSelection entry = RateSelection.create(date, time, productId, brandId);
 		
-		when(service.findMostPriorityPriceBySelection(entry)).thenReturn(TestData.PRODUCT_RATE_1);
+		when(service.findMostPriorityPriceBySelection(any(RateSelection.class))).thenReturn(TestData.PRODUCT_RATE_1);
 			
 
 		ProductRate productRate = facade.findMostPriorityPriceBySelection(date, time, productId, brandId);
@@ -44,6 +45,20 @@ class FacadeTest {
 		assertEquals(TestData.PRODUCT_RATE_1.getPrice(), productRate.getPrice());
 		assertEquals(TestData.PRODUCT_RATE_1.getPriceList(), productRate.getPriceList());
 		assertEquals(TestData.PRODUCT_RATE_1.getProductId(), productRate.getProductId());
+	}
+	
+	@Test
+	@DisplayName("petición a las 21:00 del día 14 del producto 35455 para la brand 1")
+	void checkProductRatesForDate14at21Oclock() {
+
+
+		when(service.findMostPriorityPriceBySelection(any(RateSelection.class))).thenReturn(TestData.PRODUCT_RATE_2);
+
+		ProductRate productRate = facade.findMostPriorityPriceBySelection("14-06-2020", "21:00", 35455L, 1L);
+		
+		assertEquals(TestData.PRODUCT_RATE_2.getPrice(), productRate.getPrice());
+		assertEquals(TestData.PRODUCT_RATE_2.getPriceList(), productRate.getPriceList());
+		assertEquals(TestData.PRODUCT_RATE_2.getProductId(), productRate.getProductId());
 	}
 
 
