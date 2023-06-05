df63e49b1b333bfbe0a02cf7175cfe32a609a37b
José Pascual Gimeno Marí <tirantvcia@gmail.com>
2023-06-05 23:36:03 +0200

    refactor, change package for the test

--- src/test/java/com/inditex/test/springboot/app/repositories/SpringBootJpaTest.java
+++ src/test/java/com/inditex/test/springboot/app/repositories/SpringBootJpaTest.java
@@ -1,4 +1,4 @@
-package com.inditex.test.springboot.app;
+package com.inditex.test.springboot.app.repositories;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
